c34046ee277e7bf1292eb8fd65c10039d9fbe734
Ryan Aquilina <ryanaquilina001@gmail.com>
2016-12-02 13:11:08 +0100

    Create Test Folder

--- src/main/java/PaymentProvider.java
+++ src/main/java/PaymentProvider.java
@@ -2,6 +2,5 @@
  * Created by Sterling Ryan on 25/11/2016.
  */
 public interface PaymentProvider {
-
     boolean makePayment(String bankAccount, Double amount);
 }
---
+++ src/test/java/AdPlatformTest.java
@@ -0,0 +1,44 @@
+import cucumber.deps.com.thoughtworks.xstream.mapper.Mapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by SterlingRyan on 02/12/2016.
+ */
+public class AdPlatformTest {
+    AdPlatform adPlatform;
+
+    @Before
+    public void setUp() throws Exception {
+        adPlatform = new AdPlatform();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        adPlatform = null;
+    }
+
+    @Test
+    public void registerAffiliate() throws Exception {
+        
+    }
+
+    @Test
+    public void settleAffiliateBalance() throws Exception {
+
+    }
+
+    @Test
+    public void serveAdvert() throws Exception {
+
+    }
+
+    @Test
+    public void adClicked() throws Exception {
+
+    }
+
+}
