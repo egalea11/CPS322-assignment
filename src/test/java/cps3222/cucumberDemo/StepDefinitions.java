7566d8630a0882844d510d1f6240c64ab2e882a0
Etienne Galea <egalea.11@gmail.com>
2016-12-05 14:44:49 +0100

    # init of cucumber classes

--- src/test/java/cps3222/cucumberDemo/StepDefinitions.java
+++ src/test/java/cps3222/cucumberDemo/StepDefinitions.java
@@ -1,7 +1,13 @@
 package cps3222.cucumberDemo;
 
+import static org.junit.Assert.*;
+import cucumber.api.java.en.*;
+
 /**
  * Created by Etienne G on 05/12/2016.
  */
+
 public class StepDefinitions {
+
+
 }
--- src/test/java/cps3222/cucumberTests/RunCucumberTests.java
+++ src/test/java/cps3222/cucumberTests/RunCucumberTests.java
@@ -1,7 +1,19 @@
 package cps3222.cucumberTests;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true, format = {"html:target/cucumber-html-report"})
+
 /**
- * Created by Etienne G on 05/12/2016.
+ * Created by Etienne G on 15/11/2016.
  */
+
 public class RunCucumberTests {
+
+    public class runTest {
+
+    }
 }
