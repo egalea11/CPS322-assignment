a4ab9752f2ac0c50197f4c5af31882c4a24c35d9
Etienne Galea <egalea.11@gmail.com>
2016-12-05 14:44:29 +0100

    # initialisation of cucumber classes

---
+++ src/test/java/cps3222/cucumberDemo/StepDefinitions.java
@@ -0,0 +1,7 @@
+package cps3222.cucumberDemo;
+
+/**
+ * Created by Etienne G on 05/12/2016.
+ */
+public class StepDefinitions {
+}
---
+++ src/test/java/cps3222/cucumberTests/RunCucumberTests.java
@@ -0,0 +1,7 @@
+package cps3222.cucumberTests;
+
+/**
+ * Created by Etienne G on 05/12/2016.
+ */
+public class RunCucumberTests {
+}
